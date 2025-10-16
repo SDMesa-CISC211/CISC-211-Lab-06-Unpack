45560f229cf19ed5820b62e6319ed42821b1eb61
tggbco <vito.bica@gmail.com>
2025-10-15 20:21:39 -0700

    Updated print statement to show student name on final score

--- Lab-06-unp-starter/firmware/src/main.c
+++ Lab-06-unp-starter/firmware/src/main.c
@@ -351,12 +351,12 @@ int main ( void )
             snprintf((char*)uartTxBuffer, MAX_PRINT_LEN,
                     "========= %s: Lab 6 Unpack: ALL TESTS COMPLETE, Idle Cycle Number: %ld\r\n"
                     "Summary of tests: %ld of %ld tests passed\r\n"
-                    "Final score for test cases: %ld of %ld points\r\n"
+                    "%s: Final score for test cases: %ld of %ld points\r\n"
                     "FINI!!!!!\r\n"
                     "\r\n",
                     (char *) nameStrPtr, idleCount, 
                     totalPassCount, totalTests,
-                    pointsScored, numPointsMax); 
+                    (char *) nameStrPtr, pointsScored, numPointsMax); 
 
 
 #if USING_HW 
